726e81af0eeeea74caadeb2830f9181a54333e56
Nick Bauters <nick.bauters@multimedi.be>
2020-04-21 16:28:38 +0200

    Assignment 2 done

--- src/test/java/be/learningfever/testing/thermostat/TemperatureTest.java
+++ src/test/java/be/learningfever/testing/thermostat/TemperatureTest.java
@@ -29,7 +29,45 @@ class TemperatureTest {
 
     @Test
     void testEquals() {
-        //TODO finish the equals test
+        reflexiveTest();
+        symmetricTest();
+        transitiveTest();
+    }
+
+    private void reflexiveTest() {
+        assertEquals(temp, temp, "Equals is not Reflexive");
+    }
+
+    private void symmetricTest() {
+        Temperature temp2 = new Temperature(temp.getValue());
+        assertEquals(temp.equals(temp2), temp2.equals(temp), "Equals is not Symmetric");
+    }
+
+    private void transitiveTest() {
+        Temperature temp2 = new Temperature(temp.getValue());
+        Temperature temp3 = new Temperature(temp.getValue());
+        boolean first = temp.equals(temp2);
+        boolean second = temp2.equals(temp3);
+        boolean third = temp.equals(temp3);
+        assertTrue(first && second && third, "Equals is not Transitive");
+    }
+
+    private void consistentTest() {
+        Temperature temp2 = new Temperature(temp.getValue());
+        int size = 10;
+        boolean[] checks = new boolean[size];
+        boolean[] controle = new boolean[size];
+
+        for (int i = 0; i < 10; i++) {
+            checks[i] = temp.equals(temp2);
+            controle[i] = true;
+        }
+
+        assertArrayEquals(controle, checks, "Equals is not Consistent");
+    }
+
+    private void nullTest() {
+        assertNotEquals(null, temp);
     }
 
     @Test
