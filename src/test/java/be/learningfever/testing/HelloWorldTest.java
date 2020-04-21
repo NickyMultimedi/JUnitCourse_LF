6823d889099bf29e3842c63334a44be04b0b3794
Nick Bauters <nick.bauters@multimedi.be>
2020-04-21 15:45:03 +0200

    Assignment 1 is done

--- src/test/java/be/learningfever/testing/HelloWorldTest.java
+++ src/test/java/be/learningfever/testing/HelloWorldTest.java
@@ -1,23 +1,54 @@
 package be.learningfever.testing;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 
 public class HelloWorldTest {
+    private HelloWorld hello;
 
-    @Test
-    public void testSayHello() {
-        HelloWorld hello = new HelloWorld();
-        String testValue = hello.sayHello();
-        String expectedValue = "Hello World!";
-        assertEquals(expectedValue, testValue);
+    public HelloWorldTest() {
+        System.out.println("Constructor");
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before All");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("Before each");
+        hello = new HelloWorld();
     }
 
     @Test
-    public void testSayHelloTo() {
+    @DisplayName("Saying Hello to the World")
+    void testSayHello() {
+        HelloWorld hello = new HelloWorld();
+        String testValue = hello.sayHello();
+        String expectedValue = "Hello World!";
+        assertEquals(expectedValue, testValue, "Oepsie");
+        assertNull(null);
+    }
+
+    @Test
+    @DisplayName("Saying Hello to Nick")
+    void testSayHelloTo() {
         HelloWorld hello = new HelloWorld();
         String testValue = hello.sayHelloTo("Nick");
         String expectedValue = "Hello Nick";
         assertEquals(expectedValue, testValue);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        System.out.println("After Each");
+        hello = null;
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("After All");
     }
 }
